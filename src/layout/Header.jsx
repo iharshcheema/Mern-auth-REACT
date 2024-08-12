301d998dd51c00f8e9e65155aa439ec60dbf3ed0
iharshcheema <harshcheemaweb@gmail.com>
2024-08-12 22:30:14 +0530

    ui changes

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,45 +1,38 @@
 import {
   Breadcrumb,
-  BreadcrumbEllipsis,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { TbMinusVertical } from 'react-icons/tb'
 
 const Header = () => {
   return (
-    <div className="text-center font-bold py-10 mb-10 mx-auto max-w-fit ">
+    <div className="text-center font-bold py-10 mb-10 mx-auto max-w-fit flex flex-col gap-5 text-center">
+      <h1 className="text-gray-600 font-medium mb-5">
+        MERN Authentication System
+      </h1>
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
             <BreadcrumbLink href="/">Home</BreadcrumbLink>
           </BreadcrumbItem>
-          <BreadcrumbSeparator />
+          <BreadcrumbSeparator>
+            <TbMinusVertical />
+          </BreadcrumbSeparator>
           <BreadcrumbItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger className="flex items-center gap-1">
-                <BreadcrumbEllipsis className="h-4 w-4" />
-                <span className="sr-only">Toggle menu</span>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="start">
-                <DropdownMenuItem>
-                  <a href="/about">About</a>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <BreadcrumbLink href="/about">About</BreadcrumbLink>
           </BreadcrumbItem>
-          <BreadcrumbSeparator />
+          <BreadcrumbSeparator>
+            <TbMinusVertical />
+          </BreadcrumbSeparator>
           <BreadcrumbItem>
             <BreadcrumbLink href="/register">Register</BreadcrumbLink>
           </BreadcrumbItem>
-          <BreadcrumbSeparator />
+          <BreadcrumbSeparator>
+            <TbMinusVertical />
+          </BreadcrumbSeparator>
           <BreadcrumbItem>
             <BreadcrumbLink href="/">Log-in</BreadcrumbLink>
           </BreadcrumbItem>
